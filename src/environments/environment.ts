02bec49359a3db9fff7c03a2e81bc044121a38df
a.nvlkv <a.nvlkv@protonmail.ch>
2023-10-02 20:44:45 +0700

    use dev token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  mapBoxTokenRead: 'pk.eyJ1IjoicGFja3R3byIsImEiOiJjbG4xZWRveGMxYjh0MmpudXl2OWoxNTQxIn0.pLvJq8NdqQ1vMMtFzZHQDA',
+  mapBoxTokenRead: 'pk.eyJ1IjoicGFja3R3byIsImEiOiJjbG15aTM4eHcxbWZ5MndxcGZueWJvZ3dxIn0.RHOxpcBkvecomAVq4uwcvA',
   auth0: {
     clientId: 'mRb6igqNRVdsDQnkUiy3tbhIcvy1gJn9',
     domain: 'dev-twopack-gallery.eu.auth0.com',
