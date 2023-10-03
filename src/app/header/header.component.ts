08d7d50b180ee7f298b9d9010dd9a58266f18314
a.nvlkv <a.nvlkv@protonmail.ch>
2023-10-03 09:09:48 +0700

    fix header link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  EventType,
+  Router,
+  RouterModule,
+} from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
@@ -9,6 +14,7 @@ import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { AvatarComponent } from '../avatar/avatar.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -21,22 +27,39 @@ import { AvatarComponent } from '../avatar/avatar.component';
     NzDropDownModule,
     NzButtonModule,
     NzDividerModule,
-    AvatarComponent
+    AvatarComponent,
   ],
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   @Input('card')
-  isCard?: boolean
-
+  isCard?: boolean;
 
   returnTo: string;
+  subs: Subscription[] = [];
+  logoLink = ['/', 'map'];
 
-  constructor(
-    public auth: AuthService
-  ){
-    this.returnTo = document.location.origin
+  constructor(public auth: AuthService, private router: Router) {
+    this.returnTo = document.location.origin;
+  }
+
+  ngOnInit(): void {
+    this.subs.push(
+      this.router.events.subscribe((ev) => {
+        if (ev.type === EventType.NavigationEnd) {
+          if (ev.url.endsWith('map')) {
+            this.logoLink = ['/', 'welcome'];
+          } else {
+            this.logoLink = ['/', 'map'];
+          }
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((s) => s.unsubscribe());
   }
 }
