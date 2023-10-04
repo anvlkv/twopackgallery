3b3bc5afbc8c6e5f475a140d33a0393669b9445c
a.nvlkv <a.nvlkv@protonmail.ch>
2023-10-04 09:23:52 +0700

    improve cookie consent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,43 @@
-import { Component, OnDestroy, OnInit, isDevMode } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { BrowserStorageService } from './browser-storage.service';
+import { isPlatformBrowser } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AuthModule, AuthService } from '@auth0/auth0-angular';
-import { Subscription } from 'rxjs';
 import {
   NzNotificationRef,
   NzNotificationService,
 } from 'ng-zorro-antd/notification';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { Subscription } from 'rxjs';
+import { BrowserStorageService } from './browser-storage.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 const COOKIE_CONSENT = 'hasGivenCookieConsent';
 @Component({
   standalone: true,
-  imports: [
-    RouterModule,
-    HttpClientModule,
-    AuthModule,
-    ServiceWorkerModule
-  ],
+  imports: [RouterModule, HttpClientModule, AuthModule, ServiceWorkerModule, NzButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   title = 'twopack.gallery';
   subs: Subscription[] = [];
 
   constructor(
     private auth: AuthService,
     private notification: NzNotificationService,
-    private storage: BrowserStorageService
+    private storage: BrowserStorageService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
   notificationRef?: NzNotificationRef;
@@ -49,17 +55,28 @@ export class AppComponent implements OnInit, OnDestroy {
       })
     );
 
-    if (!this.storage.get(COOKIE_CONSENT)) {
-      this.notification
-        .info('Cookies...', 'We use 3rd-party cookies for authentication.', {
-          nzDuration: 0,
+  }
+
+  @ViewChild('okCookie')
+  okCookie!: TemplateRef<any>
+  
+  ngAfterViewInit(): void {
+    if (
+      !this.storage.get(COOKIE_CONSENT) &&
+      isPlatformBrowser(this.platformId)
+    ) {
+      this.subs.push(this.notification
+      .info('Cookies...', 'We use 3rd-party cookies for authentication.', {
+        nzDuration: 0,
           nzPlacement: 'bottomLeft',
-          nzCloseIcon: 'check-circle',
+          nzCloseIcon: this.okCookie,
+          
         })
         .onClose.subscribe(() => {
           this.storage.set(COOKIE_CONSENT, true);
-        });
+        }));
     }
+    
   }
 
   ngOnDestroy(): void {
